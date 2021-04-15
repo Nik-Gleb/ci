255eec77372e2303d8b6025b6710bcafd9f2c17f
Никитенко Глеб Викторович (4034637) <gnikitenko@dev.vtb.ru>
2021-04-15 09:30:20 +0300

    Full script

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "Hello, World!" >> artifact.txt
+apk=./output.apk
+values=values
+manifest=AndroidManifest.xml
+filename=$(basename -- "$1")
+temp=$(basename "$1" .xml)
+mkdir $values && cp $1 ./$values
+aapt2 compile ./$values/$filename -o ./$temp -v && rm -r ./$values && unzip ./$temp && rm ./$temp
+echo "<manifest xmlns:android='http://schemas.android.com/apk/res/android' package='com.example.shimmers'/>" >> ./$manifest
+ver_temp=${ANDROID_BUILD_TOOLS_VERSION%.*}
+version=${ver_temp%.*}
+flat="./${values}_${temp}.arsc.flat"
+aapt2 link $flat -o $apk -I $ANDROID_HOME/platforms/android-$version/android.jar --manifest ./$manifest --stable-ids stableIds.txt
+rm ./$manifest && rm $flat && unzip $apk && rm $apk && rm ./$manifest

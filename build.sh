26bf72aeb3cf8b30d544014b5859d80ecc72177e
Никитенко Глеб Викторович (4034637) <gnikitenko@dev.vtb.ru>
2021-04-15 12:54:24 +0300

    Full script

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-ANDROID_BUILD_TOOLS_VERSION=30.0.3
 apk=./output.apk
 values=values
+resources=resources.xml
+ids=ids.txt
 manifest=AndroidManifest.xml
-filename=$(basename -- "$1")
-temp=$(basename "$1" .xml)
-mkdir $values && cp $1 ./$values
-${ANDROID_HOME}/build-tools/${ANDROID_BUILD_TOOLS_VERSION}/aapt2 compile ./$values/$filename -o ./$temp -v && rm -r ./$values && unzip ./$temp && rm ./$temp
+filename=$(basename -- "$resources")
+temp=$(basename "$resources" .xml)
 echo "<manifest xmlns:android='http://schemas.android.com/apk/res/android' package='com.example.shimmers'/>" >> ./$manifest
+echo $1 >> ./$resources
+echo $2 >> ./$ids
+mkdir $values && cp $resources ./$values
+${ANDROID_HOME}/build-tools/${ANDROID_BUILD_TOOLS_VERSION}/aapt2 compile ./$values/$filename -o ./$temp -v && rm -r ./$values && unzip ./$temp && rm ./$temp
 ver_temp=${ANDROID_BUILD_TOOLS_VERSION%.*}
 version=${ver_temp%.*}
 flat="./${values}_${temp}.arsc.flat"
-echo $2
-${ANDROID_HOME}/build-tools/${ANDROID_BUILD_TOOLS_VERSION}/aapt2 link $flat -o $apk -I $ANDROID_HOME/platforms/android-$version/android.jar --manifest ./$manifest --stable-ids stableIds.txt
+${ANDROID_HOME}/build-tools/${ANDROID_BUILD_TOOLS_VERSION}/aapt2 link $flat -o $apk -I $ANDROID_HOME/platforms/android-$version/android.jar --manifest ./$manifest --stable-ids $ids
 rm ./$manifest && rm $flat && unzip $apk && rm $apk && rm ./$manifest
